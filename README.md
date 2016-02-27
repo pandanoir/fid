6ea2da93ecfd3326109897d84e202ce64aca18d6
pandanoir <moon_in_france@yahoo.co.jp>
2016-02-27 12:44:52 +0900

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# fid
-Fully Indexable Dictionary
+# FID
+This library provides FID(Fully Indexable Dictionary) in JavaScript.
+
+## Example
+```javascript
+var myfid = new FID([0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1]);
+var n = 5;
+myfid.rank(n); // 3
+myfid.select(3); // 5
+```
+
+## rank()
+rank(n) returns the number of 1 up to position n.
+
+## select()
+select(n) returns the position of *n*th 1. (Note: the position which select returns isn't equal to index of Array. the position add 1 is equal to index of Array.)
---
+++ fid.js
@@ -0,0 +1,81 @@
+'use strict';
+class FID {
+    constructor(arr) {
+        this.rankData = createRankData(arr);
+        this.smallRankData = createSmallRankData(arr);
+        this.bitTable = createBitTable(arr);
+        this.rawArray = arr;
+        this.bigBlockSize = Math.ceil(Math.log(arr.length) * Math.log(arr.length));
+        this.smallBlockSize = Math.ceil(Math.log(arr.length) / 2);
+    }
+    rank(n) {
+            var bigBlockIndex = Math.floor((n - 1) / this.bigBlockSize);
+            var smallBlockIndex = bigBlockIndex * Math.ceil(this.bigBlockSize / this.smallBlockSize) + Math.floor((n - 1) % this.bigBlockSize / this.smallBlockSize);
+            var i3 = zerofill(
+                this.rawArray.slice(bigBlockIndex * this.bigBlockSize + (smallBlockIndex - bigBlockIndex * Math.ceil(this.bigBlockSize / this.smallBlockSize)) * this.smallBlockSize, n).join(''),
+                this.smallBlockSize);
+        return this.rankData[bigBlockIndex] +
+            this.smallRankData[smallBlockIndex] +
+            this.bitTable[i3];
+    }
+    select(n) {
+        var len = this.rawArray.length;
+        var start = 1, end = len;
+        while (start < end) {
+            var mid = 0 | (start + end) / 2;
+            if (this.rank(mid) < n) start = mid + 1;
+            else if (this.rank(mid) >= n) end = mid;
+        }
+        if (this.rank(start) === n) return start;
+        return -1;
+    }
+}
+function createRankData(arr) {
+    var len = Math.ceil(Math.log(arr.length) * Math.log(arr.length));
+    var res = [0];
+    for (var i = 0, _i = arr.length; i - len < _i; i += len) {
+        res.push(res[res.length - 1] + arr.slice(i, i + len).filter(isOne).length);
+    }
+    return res;
+}
+function createSmallRankData(arr) {
+    var len = Math.ceil(Math.log(arr.length) / 2);
+    var blockLen = Math.ceil(Math.log(arr.length) * Math.log(arr.length));
+    var res = [];
+    for (var i = 0, _i = arr.length; i - len < _i; i += blockLen) {
+        for (var j = 0; j < blockLen; j += len) {
+            res.push(
+                arr.slice(i, i + Math.min(j, blockLen)).filter(isOne).length
+            );
+        }
+    }
+    return res;
+}
+function createBitTable(arr) {
+    var len = Math.ceil(Math.log(arr.length) / 2);
+    var res = {};
+    for (var i = 0, _i = Math.pow(2, len); i < _i; i = 0 | i + 1) {
+        res[zerofill(i.toString(2), len)] = count(i.toString(2), '1');
+    }
+    return res;
+}
+function count(string, key) {
+    var counter = 0;
+    for (var i = 0, _i = string.length; i < _i; i = 0 | i + 1) {
+        if (string.charAt(i) === key) {
+            counter = 0 | counter + 1;
+        }
+    }
+    return counter;
+}
+function zerofill(str, len) {
+    var zeros = '000';
+    while (zeros.length <= len) {
+        zeros = zeros + zeros;
+    }
+    return (zeros + str).slice(-len);
+}
+function isOne(n) {
+    return n === 1 || n === '1';
+}
+module.exports = FID;
